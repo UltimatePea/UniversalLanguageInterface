d041f7bde731878db15f19d9ff21e2156c43e1f6
Ultimate Pea <superchenzb@gmail.com>
2017-09-05 16:05:51 -0700

    Add Node Js initial packages

---
+++ nodejs/functions.js
@@ -0,0 +1,10 @@
+module.exports = {
+	callSingle: callSingle,
+	exportAndStart: exportAndStart
+}
+
+function callSingle(progName, cmdargs, functionName, args) {
+}
+
+function exportAndStart(map) {
+}
